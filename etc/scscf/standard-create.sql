b2b5c79f3f459d3235b088352ca682b013ee42e7
nathanfe97 <nathanhuynh97@gmail.com>
2019-07-17 21:53:35 +0700

    init

---
+++ etc/scscf/standard-create.sql
@@ -0,0 +1,9 @@
+use scscf;
+CREATE TABLE IF NOT EXISTS `version` (
+    `table_name` VARCHAR(32) NOT NULL,
+    `table_version` INT UNSIGNED DEFAULT 0 NOT NULL,
+    CONSTRAINT table_name_idx UNIQUE (`table_name`)
+);
+
+INSERT INTO version (table_name, table_version) values ('version','1');
+
---
+++ sbin/runims.sh
@@ -0,0 +1,3 @@
+/kamailio-5.2/sbin/kamailio -f /kamailio-5.2/etc/pcscf/kamailio.cfg
+/kamailio-5.2/sbin/kamailio -f /kamailio-5.2/etc/scscf/kamailio.cfg
+/kamailio-5.2/sbin/kamailio -f /kamailio-5.2/etc/icscf/kamailio.cfg
